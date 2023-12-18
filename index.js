31ef5b813a457f96bea96545bf169424ed3ccc5d
Maria Kurmacheva <tscherkasova.mascha@gmail.com>
2023-12-18 22:52:50 +0500

    share screenshot

--- index.js
+++ index.js
@@ -1,35 +1,38 @@
 function share() {
-    console.log('AAAA');
-    setImage();
+    const node = document.getElementById('node');
 
-    if (navigator.canShare) {
-        const shareData = {
-            title: 'test',
-            url: 'https://www.google.com/'
-        }
+    domtoimage.toBlob(node)
+        .then(function (blob) {
+            const file = new File([blob], "PrettyPicture.png", {
+                type: "image/png"
+            });
+            const filesArray = [file];
+            let shareData = { files: filesArray };
 
-        navigator.share(shareData)
-            .then(() => alert("Share was successful"))
-            .catch((error) =>
-                alert("Sharing failed" + error)
-            );
-    }
+            if (navigator.canShare && navigator.canShare(shareData)) {
+                shareData.text = "Смотри как я могу! Ты тоже так можешь тут";
+                shareData.url = "https://google.com";
+
+                navigator
+                    .share(shareData)
+                    .then(() => alert("Share was successful"))
+                    .catch((error) =>
+                        alert("Sharing failed" + error)
+                    );
+            } else {
+                alert("Your system doesn't support sharing files");
+            }
+
+
+        })
+        .catch(function (error) {
+            console.error('oops, something went wrong!', error);
+        });
 }
 
 function init() {
     const button = document.getElementById('shareButton');
     button.addEventListener("click", share);
-}
-
-function setImage() {
-    const img = document.querySelectorAll('meta[property=og\\:image]')[0];
-    const value = Math.random() < 0.5
-        ? 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBksqziEUqmY9ovuHYd2LGU_7EmRx8wsR_WEz3ACkApyf3rGDZBhtjwa_ZVQW4K9oL2uA&usqp=CAU'
-        : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShzt3IYxgnU1D9hJ8OZMZy8KqJLcPZpOT9-6HEfM1ZiEbahAn0h3oCN9Pl8_TowMZZpiY&usqp=CAU';
-
-    console.log('value', value);
-
-    img.setAttribute('content', value)
 }
 
 init();
